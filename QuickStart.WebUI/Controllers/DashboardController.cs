3b022ad64bde4ebdd794aac6601c6824485a2cd3
busenurdmb <bus.dmb@gmail.com>
2026-02-20 22:33:01 +0300

    staticlik çekildi

--- QuickStart.WebUI/Controllers/DashboardController.cs
+++ QuickStart.WebUI/Controllers/DashboardController.cs
@@ -4,8 +4,24 @@ namespace QuickStart.WebUI.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DashboardController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7051/api/Testimonial/TestimonialCount");
+            var JsonData=await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.TestimonialCount = JsonData;
+
+            var responseMessage1 = await client.GetAsync("https://localhost:7051/api/Service/ServiceCount");
+            var JsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            ViewBag.ServiceCount = JsonData1;
             return View();
         }
     }
